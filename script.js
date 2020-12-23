0e0f44841b54372ac32ce5bb607c12bca2d2b1e4
Mason M <m8mamiya@gmail.com>
2020-12-23 09:55:08 -0800

    add css styling start script page

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+var searchButton = $(".searchButton");
 var APIKey = "00d372565b9be433ab31f424c1839876";
-var City = 
+
 queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
 City + "&appid=" + APIKey;
+
+// Forloop for index page
+for (var i = 0; i < localStorage.length; i++) {
+
+    var city = localStorage.getItem(i);
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>");
+}
